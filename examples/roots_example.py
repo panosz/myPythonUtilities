0c908985ce019d9599e3f99bbc257027204705aa
Panagiotis Zestanakis <panosz@gmail.com>
2019-03-13 14:03:43 +0200

    find_iso_points returns a named tuple of type Level_Cross

--- examples/roots_example.py
+++ examples/roots_example.py
@@ -1,38 +1,38 @@
 import numpy as np
 import sys
 import os
-my_path = os.path.abspath(os.path.join(os.path.dirname(__file__),".."))
-print(my_path)
-sys.path.insert(1,my_path)
+
+my_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.insert(1, my_path)
 
 import panos_utilities.roots as roots
 
-
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
-
-    my_window = (0, 10)
 
 
     def fun_to_solve(x):
         return np.sin(2 * np.pi * x)
 
 
-    print(roots.version)
+    def sample_fun(f, window, n_samples):
+        x0, x1 = window
+        x = np.linspace(x0, x1, n_samples)
+        return (x, f(x))
+
+
+    print('Using roots version {}'.format(roots.version))
 
     level = 0.5
+    window = (0, 10)
 
-    result = roots.find_iso_points(fun_to_solve, iso_level=level, window=my_window)
+    result = roots.find_iso_points(fun_to_solve, level, window)
 
-    x_iso = [sol.root for sol in result]
-    y_iso = [level] * len(x_iso)
+    x_iso = [sol.root for sol in result.solutions]
+    y_iso = [result.level] * len(x_iso)
 
-    x0, x1 = my_window
-
-    x = np.linspace(x0, x1, 1000)
-    y = fun_to_solve(x)
+    x, y = sample_fun(fun_to_solve, window, n_samples=1000)
 
     plt.plot(x, y, 'b-')
     plt.plot(x_iso, y_iso, 'rx')
     plt.show()
-
--- panos_utilities/roots.py
+++ panos_utilities/roots.py
@@ -1,25 +1,28 @@
 from . import adjacent_numpy as adn
+from collections import namedtuple
 import numpy as np
 import scipy.optimize as optimize
 
-
-version = '0.1'
+version = '0.1.1'
 
 
 def find_root(f, window, n_samples=100):
-    """
+    '''
     find the roots of a scalar function within an specified interval.
     :param f: callable with float or numpy.array input
     :param window: tuple (xmin,xmax) defining the interval in which to search for roots
     :param n_samples: int, optional. number of samples distributed uniformly in window
     :return: sol: list of scipy.optimize.RootResults
-    """
+    '''
     x0, x1 = window
     x = np.linspace(x0, x1, n_samples)
     y = f(x)
-    x_before, x_after, _, _ = adn.zero_cross_elems(x, y)
+    cross = adn.zero_cross_elems(x, y)
 
-    return [optimize.root_scalar(f, bracket=bra_ket) for bra_ket in zip(x_before, x_after)]
+    return [optimize.root_scalar(f, bracket=bra_ket) for bra_ket in zip(cross.x_before, cross.x_after)]
+
+
+Level_Cross = namedtuple('LevelCross', ['level', 'solutions'])
 
 
 def find_iso_points(f, iso_level, *args, **kwargs):
@@ -29,11 +32,14 @@ def find_iso_points(f, iso_level, *args, **kwargs):
     :param iso_level: float, horizontal level
     :param args: forwarded to find_root
     :param kwargs: forwarded to find_root
-    :return: sol: list of scipy.optimize.RootResults
+    :return: Level_Cross: a namedtuple with members
+        level = float the iso level that is crossed and
+        solutions = list of scipy.optimize.RootResults
     """
+
     def f_moved(*args_inner, **kwargs_inner):
         return f(*args_inner, **kwargs_inner) - iso_level
 
-    return find_root(f_moved, *args, **kwargs)
+    root_list = find_root(f_moved, *args, **kwargs)
 
-
+    return Level_Cross(iso_level,root_list)
