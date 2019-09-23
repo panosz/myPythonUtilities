db7df88d18a2b66c1aa609852b6553bc8194eb8e
Panagiotis Zestanakis <panosz@gmail.com>
2019-09-23 17:58:42 +0300

    no more hacks to include the local package.
    use venv instead

--- examples/roots_example.py
+++ examples/roots_example.py
@@ -1,35 +1,27 @@
 import numpy as np
-import sys
-import os
-
-my_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-sys.path.insert(1, my_path)
-
 import panos_utilities
 import panos_utilities.roots as roots
-
-if __name__ == '__main__':
-    import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 
-    def fun_to_solve(x):
-        return np.sin(2 * np.pi * x)
+def fun_to_solve(x):
+    return np.sin(2 * np.pi * x)
 
 
-    print('Using panos_utilities version {}'.format(panos_utilities.version))
-    print('Using roots version {}'.format(roots.version))
+print('Using panos_utilities version {}'.format(panos_utilities.version))
+print('Using roots version {}'.format(roots.version))
 
-    level = [0.5, -0.4, 0, 1.2]
-    window = (0, 10)
+level = [0.5, -0.4, 0, 1.2]
+window = (0, 10)
 
-    x, y = roots.sample_fun(fun_to_solve, window, n_samples=1000)
-    plt.plot(x, y, 'b-')
+x, y = roots.sample_fun(fun_to_solve, window, n_samples=1000)
+plt.plot(x, y, 'b-')
 
-    results = roots.find_roots(fun_to_solve, level, window)
+results = roots.find_roots(fun_to_solve, level, window)
 
-    for result in results:
-        x_iso = result.solutions
-        y_iso = [result.level] * len(x_iso)
-        plt.plot(x_iso, y_iso, 'rx')
+for result in results:
+    x_iso = result.solutions
+    y_iso = [result.level] * len(x_iso)
+    plt.plot(x_iso, y_iso, 'rx')
 
-    plt.show()
+plt.show()
